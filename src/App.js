1db55a68b3a81b71fa7ef0fa22fa7aa1c52f36d4
nejc000l <nejc.lampic@gmail.com>
2022-05-18 00:20:10 +0200

    dodani filtri pa buttons

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MovieCard from "./components/MovieCard";
+import Navbar from "./components/Navbar";
+import Filter from "./components/Filter";
+import Button from './components/Button';
+function App() {
+  const BASE_URL = "https://api.themoviedb.org/3";
+  const [movies, setMovies] = useState([]);
+  const [filterd, setFilterd] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+
+  const getApi = async () => {
+    const {
+      data: { results },
+    } = await axios.get(`${BASE_URL}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+      },
+    });
+    setMovies(results);
+    setFilterd(results);
+  };
+  useEffect(() => {
+    getApi();
+  }, []);
+  
+  const showMovies = () =>
+  filterd.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <div className="container-wrapper">
+          <div className="title">Popular Movies</div>
+          <div className="container-content">
+            <div className="filter">
+            <Filter
+                movies={movies}
+                setFilterd={setFilterd}
+                activeGenre={activeGenre}
+                setActiveGenre={setActiveGenre}
+              />
+            </div>
+            <div>
+              <div className="container">{showMovies()}</div>
+              <Button/>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style/Navbar.css";
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar fixed-top navbar expand-lg">
+     
+    </nav>
+  );
+};
+
+export default Navbar;
