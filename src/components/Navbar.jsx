913931ec5192403cb1617e312bdbfad30069d70e
nejc000l <nejc.lampic@gmail.com>
2022-05-18 16:39:37 +0200

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,29 +3,48 @@ import "./style/Navbar.css";
 import { FaSearch } from "react-icons/fa";
 import { AiOutlineSetting, AiOutlinePlus,AiOutlineBell} from "react-icons/ai";
 import {RiEnglishInput} from 'react-icons/ri'
+import {navItems} from './data/NavItems'
+import Dropdown from './DropdownNav'
+
 const Navbar = () => {
-  const style = { fill: "#fff", fontSize: "1.5em" }
+  const style = { fill: "#fff", fontSize: "1.5em", cursor: "pointer"}
 
   return (
     <nav className="nav nav__blue">
     <div className="nav-container">
    
     <div className="nav-list">
-    <ul>
-        <li>Movies</li>
-        <li>TV Shows</li>
-        <li>People</li>
-        <li>More</li>
-      </ul>
+    <div>
+      <a>
+        <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg" alt="The Movie Database (TMDB)" width="154" height="20"/>      
+      </a>
     </div>
+    <div className="nav-item">
+    <ul>
+       {navItems.map((item)=>{
+          return(
+            <li key={item.id} className="item.cName">
+             <a href={item.path}>{item.title}</a>
+            </li>
+          )
+        })}
+
+      </ul>
+      </div>
+    </div>
+
     <div className="icons-list">
     <AiOutlinePlus className="icon" style={style} />
     <RiEnglishInput className="icon" style={style} />
     <AiOutlineBell className="icon" style={style} />
-    <AiOutlineSetting className="icon" fill='#01b4e4'/>
-    <FaSearch className="icon" fill="#01b4e4"  />
+    <AiOutlineSetting className="icon" fill='#01b4e4' cursor='pointer' />
+    <FaSearch className="icon" fill="#01b4e4" cursor='pointer'  />
     </div>
       </div>
+      <div className="container-box">
+
+      </div>
+
     </nav>
   );
 };
---
+++ src/components/data/NavItems.js
@@ -0,0 +1,114 @@
+export const navItems = [
+  {
+    id: 1,
+    title: "Movies",
+    path: "./",
+    cName: "nav-item",
+  },
+  {
+    id: 2,
+    title: "TV Shows",
+    path: "./services",
+    cName: "nav-item",
+  },
+  {
+    id: 3,
+    title: "People",
+    path: "./products",
+    cName: "nav-item",
+  },
+  {
+    id: 4,
+    title: "More",
+    path: "./contactus",
+    cName: "nav-item",
+  },
+];
+
+export const moviesDropdown = [
+  {
+    id: 1,
+    title: "Popular",
+    path: "./marketing",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Now Playing",
+    path: "./consulting",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "Upcoming",
+    path: "./design",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "Top Rated",
+    path: "./development",
+    cName: "submenu-item",
+  },
+];
+
+export const tvDropdown = [
+  {
+    id: 1,
+    title: "Popular",
+    path: "./marketing",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Airing Today",
+    path: "./consulting",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "On Tv",
+    path: "./design",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "Top Rated",
+    path: "./development",
+    cName: "submenu-item",
+  },
+];
+export const popularDropdown = [
+  {
+    id: 1,
+    title: "Popular People",
+    path: "./marketing",
+    cName: "submenu-item",
+  }
+];
+export const moreDropdown = [
+  {
+    id: 1,
+    title: "Discussion",
+    path: "./marketing",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Laderboard",
+    path: "./consulting",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "./design",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "API",
+    path: "./development",
+    cName: "submenu-item",
+  },
+];
